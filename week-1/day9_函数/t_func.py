a4de96cc43290dd8b06e7a45f0a85f7a21c30843
zhuleilt <zhulei@k12cloud.cn>
2019-12-11 10:31:30 +0800

    复习第一模块

---
+++ week-1/day9_函数/t_func.py
@@ -0,0 +1,18 @@
+s1 = "asdadqeqeqgxcvhrtgh"
+
+# python没有len
+
+index = 0
+for i in s1:
+    index += 1
+
+print(index)
+
+# def my_len()
+
+ll = [1, 2, 3, 4, 5, 6, 7]
+index = 0
+for i in ll:
+    index += 1
+
+print(index)
