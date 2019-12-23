9a5cbff1ffb00edd948fd41f76804542b1398f7e
zhuleilt <zhulei@k12cloud.cn>
2019-12-23 09:32:30 +0800

    匿名函数

--- week-2/day13/匿名函数lambda.py
+++ week-2/day13/匿名函数lambda.py
@@ -12,7 +12,7 @@ func1 = lambda a, b: a + b
 print(func1(1,2))
 
 # 接收一个可切片的数据，返回索引为0与2的对应元素（元祖形式）
-func2 = lambda arg: tuple(arg[0:2])
+func2 = lambda arg: (arg[0],arg[2])
 
 # 写匿名函数：接收两个int参数，将较大的数据返回
 func3 = lambda a,b: a if a > b else b
